3e8cb51675a1b558316481995b4481f10df85b1b
Aayushi Sharma <aayushisharma20@navgurukul.org>
2024-12-14 19:36:45 +0530

    Changes codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,93 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import FullCalendar from "@fullcalendar/react";
-// import dayGridPlugin from "@fullcalendar/daygrid";
-// import interactionPlugin from "@fullcalendar/interaction";
-// import EventModal from "./EventModal";
-// import "./App.css";
-
-// const App = () => {
-//   const [events, setEvents] = useState([]);
-//   const [modalOpen, setModalOpen] = useState(false);
-//   const [selectedDate, setSelectedDate] = useState(null);
-//   const [selectedEvent, setSelectedEvent] = useState(null);
-
-//   useEffect(() => {
-//     const savedEvents = JSON.parse(localStorage.getItem("events")) || [];
-//     setEvents(savedEvents);
-//   }, []);
-
-//   useEffect(() => {
-//     localStorage.setItem("events", JSON.stringify(events));
-//   }, [events]);
-
-//   const handleDateClick = (info) => {
-//     setSelectedDate(info.dateStr);
-//     setSelectedEvent(null);
-//     setModalOpen(true);
-//   };
-
-//   const handleEventClick = (info) => {
-//     const clickedEvent = events.find((event) => event.id === info.event.id);
-//     setSelectedEvent(clickedEvent);
-//     setModalOpen(true);
-//   };
-
-//   const addEvent = (newEvent) => {
-//     setEvents([...events, { ...newEvent, id: Date.now().toString() }]);
-//     setModalOpen(false);
-//   };
-
-//   const updateEvent = (updatedEvent) => {
-//     setEvents(
-//       events.map((event) =>
-//         event.id === updatedEvent.id ? updatedEvent : event
-//       )
-//     );
-//     setModalOpen(false);
-//   };
-
-//   const deleteEvent = (eventId) => {
-//     setEvents(events.filter((event) => event.id !== eventId));
-//     setModalOpen(false);
-//   };
-
-//   return (
-//     <div className="App">
-//   <div className="full-calendar-container">
-//     <FullCalendar
-//       plugins={[dayGridPlugin, interactionPlugin]}
-//       initialView="dayGridMonth"
-//       events={events}
-//       dateClick={handleDateClick}
-//       eventClick={handleEventClick}
-//     />
-//   </div>
-
-//   {modalOpen && (
-//     <EventModal
-//       selectedDate={selectedDate}
-//       selectedEvent={selectedEvent}
-//       onClose={() => setModalOpen(false)}
-//       onAddEvent={addEvent}
-//       onUpdateEvent={updateEvent}
-//       onDeleteEvent={deleteEvent}
-//     />
-//   )}
-// </div>
-
-//   );
-// };
-
-// export default App;
-
-
-
-
-
-
-
-
-
 import React, { useState, useEffect } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
@@ -101,13 +11,11 @@ const App = () => {
   const [selectedDate, setSelectedDate] = useState(null);
   const [selectedEvent, setSelectedEvent] = useState(null);
 
-  // This useEffect will run once on component mount and load events from localStorage
   useEffect(() => {
     const savedEvents = JSON.parse(localStorage.getItem("events")) || [];
     setEvents(savedEvents);
   }, []);
 
-  // This useEffect will run whenever the events state changes and save to localStorage
   useEffect(() => {
     localStorage.setItem("events", JSON.stringify(events));
   }, [events]);
@@ -145,27 +53,28 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="full-calendar-container">
-        <FullCalendar
-          plugins={[dayGridPlugin, interactionPlugin]}
-          initialView="dayGridMonth"
-          events={events}
-          dateClick={handleDateClick}
-          eventClick={handleEventClick}
-        />
-      </div>
+  <div className="full-calendar-container">
+    <FullCalendar
+      plugins={[dayGridPlugin, interactionPlugin]}
+      initialView="dayGridMonth"
+      events={events}
+      dateClick={handleDateClick}
+      eventClick={handleEventClick}
+    />
+  </div>
 
-      {modalOpen && (
-        <EventModal
-          selectedDate={selectedDate}
-          selectedEvent={selectedEvent}
-          onClose={() => setModalOpen(false)}
-          onAddEvent={addEvent}
-          onUpdateEvent={updateEvent}
-          onDeleteEvent={deleteEvent}
-        />
-      )}
-    </div>
+  {modalOpen && (
+    <EventModal
+      selectedDate={selectedDate}
+      selectedEvent={selectedEvent}
+      onClose={() => setModalOpen(false)}
+      onAddEvent={addEvent}
+      onUpdateEvent={updateEvent}
+      onDeleteEvent={deleteEvent}
+    />
+  )}
+</div>
+
   );
 };
 
